c83fb6edaa60d5b59d0ded120ccefd903fe0b534
Jotism <jota02349@gmail.com>
2025-04-27 16:36:35 -0300

    Estado inicial del proyecto

--- app/Views/plantilla/Header.php
+++ app/Views/plantilla/Header.php
@@ -10,59 +10,31 @@
     <link href="assets/css/miestilo.css" rel="stylesheet" >
   </head>
   <body class="bg-primary">
-    <div class="bg-secondary">
-      <a href="/proyecto_mazzanti_marcos"><img src="assets/img/logo-xport-color.png" class="rounded mx-auto d-block img-logo-menu"></a>
-    </div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-      
-      <div class="container-fluid">
-        
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarCenteredExample"
-          aria-controls="navbarCenteredExample"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i class="fas fa-bars"></i>
-        </button>
 
-        <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
-          
-          <ul class="navbar-nav mb-2 mb-lg-0">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Catalogo
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Marca 1</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Marca 2</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Marca 3</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Marca 4</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Marca 5</a></li>
-              </ul>
+    <nav class="navbar navbar-expand-lg bg-secondary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/proyecto_mazzanti_marcos"><img src="assets/img/logo-xport-blanco.png" class="img-logo-menu"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active text-light" aria-current="page" href="/proyecto_mazzanti_marcos">Inicio</a>
             </li>
             <li class="nav-item">
               <a class="nav-link text-light" href="/proyecto_mazzanti_marcos/Quienes-Somos">Quienes Somos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-light" href="/proyecto_mazzanti_marcos/Terminos-Y-Uso">Terminos Y Usos</a>
+              <a class="nav-link text-light" href="/proyecto_mazzanti_marcos/Terminos-Y-Uso">Términos y Usos</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled text-light" aria-disabled="true">Consultas</a>
+              <a class="nav-link text-light" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">Comercialización</a>
             </li>
           </ul>
-          
         </div>
-        
       </div>
-      
     </nav>
-
-    
