5e79041d9e93548a43064c551f9121b1afc6f5ae
Marcos <marcossantinomazzanti@gmail.com>
2025-04-28 23:04:44 -0300

    Formulario contacto funcional

--- app/Views/Contacto.php
+++ app/Views/Contacto.php
@@ -5,7 +5,21 @@
     <p><strong>¿Querés ponerte en contacto con nosotros?</strong></p>
     <p>En Xport estamos para ayudarte a encontrar el auto de tus sueños. 
        Si tenés consultas, querés agendar una visita o simplemente necesitás más información, podés comunicarte a través de los siguientes medios:</p>
-    
+
+    <div class="form-container">
+    <form action="https://api.web3forms.com/submit" method="POST" class="contact-right">
+        <div class="contact-right-title">
+        <h2 class = "titulos-contacto">Contactanos</h2>
+        <hr>
+        </div>
+        <input type="hidden" name="access_key" value="bf818953-ca3a-4d60-8224-8346c9b5770d">
+        <input type="text" name="Nombre" placeholder="Tu nombre" class="contact-inputs" required>
+        <input type="email" name="Correo" placeholder="Tu correo" class="contact-inputs" required>
+        <textarea name="message" placeholder="Tu consulta" class="contact-inputs" required></textarea>
+        <button type="submit">Enviar</button>
+    </form>
+    </div>
+
 <br>
     <h2 class = "titulos-contacto">Medios de Contacto</h2>
     <ul style="list-style: none; padding: 0;">
