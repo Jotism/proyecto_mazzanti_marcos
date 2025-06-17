30d5bf78370c2da6392e7cf243f1d2e0b065e159
Jotism <jota02349@gmail.com>
2025-06-17 14:17:32 -0300

    modelos de productos y categorias

---
+++ app/Models/Productos_model.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class Productos_model extends Model
+{
+    protected $table = 'productos';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+                                'nombre_prod', 
+                                'imagen', 
+                                'categoria_id', 
+                                'precio', 
+                                'precio_vta', 
+                                'stock', 
+                                'stock_min', 
+                                'eliminado'
+                               ];
+}
