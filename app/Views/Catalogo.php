2a224cc3a776accdb044844b6685d718f91eeded
Jotism <jota02349@gmail.com>
2025-06-19 22:53:48 -0300

    cambio de estilo en crud de usuarios y catalogo

--- app/Views/Catalogo.php
+++ app/Views/Catalogo.php
@@ -15,29 +15,33 @@
     <!-- Mostrar productos -->
     <div class="row">
         <?php if (!empty($productos)): ?>
-            <?php foreach ($productos as $prod): ?>
-                <div class="col-md-4 mb-4">
-                    <div class="card h-100">
-                        <?php if (!empty($prod['imagen'])): ?>
-                            <img src="<?= base_url('uploads/' . $prod['imagen']) ?>" alt="Imagen del producto"
-                                 class="card-img-top img-thumbnail" style="object-fit: cover; width: 100%; height: 200px;">
-                        <?php endif; ?>
-                        <div class="card-body d-flex flex-column">
-                            <h5 class="card-title"><?= $prod['nombre_prod'] ?></h5>
-                            <p class="card-text"><?= $prod['precio'] ?> USD</p>
+           <?php foreach ($productos as $row) { ?>
+                                <div class="col-md-4 mb-4">
+                                    <div class="card h-100">
+                                        <img src="<?= base_url('assets/uploads/' . $row['imagen']) ?>" class="card-img-top" alt="<?= $row['nombre_prod'] ?>" style="height: 200px; object-fit: cover;">
+                                        <div class="card-body">
+                                            <h5 class="card-title"><?= $row['nombre_prod'] ?></h5>
+                                            <p class="card-text">$<?= number_format($row['precio_vta'], 2, ',', '.') ?></p>
 
-                            <!-- Botón Agregar al carrito -->
-                            <form action="<?= base_url('carrito/add') ?>" method="post" class="mt-auto">
-                                <input type="hidden" name="id" value="<?= $prod['id'] ?>">
-                                <input type="hidden" name="nombre" value="<?= $prod['nombre_prod'] ?>">
-                                <input type="hidden" name="precio" value="<?= $prod['precio'] ?>">
-                                <input type="number" name="cantidad" value="1" min="1" class="form-control mb-2">
-                                <button type="submit" class="btn btn-primary w-100">Agregar al carrito</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+                                            <?= form_open('carrito/add'); ?>
+                                                <?= form_hidden('id', $row['id']); ?>
+                                                <?= form_hidden('precio_vta', $row['precio_vta']); ?>
+                                                <?= form_hidden('nombre_prod', $row['nombre_prod']); ?>
+                                                <?= form_hidden('imagen', $row['imagen']); ?>
+
+                                                <?php
+                                                $btn = array(
+                                                    'class' => 'btn btn-primary btn-block fuenteBotones',
+                                                    'value' => 'Agregar al Carrito',
+                                                    'name' => 'action'
+                                                );
+                                                echo form_submit($btn);
+                                                echo form_close();
+                                                ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
         <?php else: ?>
             <p class="text-center">No hay productos disponibles.</p>
         <?php endif; ?>
--- app/Views/productos/Vista_productos.php
+++ app/Views/productos/Vista_productos.php
@@ -57,3 +57,14 @@
         <div class="alert alert-info">No hay productos cargados.</div>
     <?php endif; ?>
 </div>
+
+<!-- Scripts de jQuery y DataTables -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+<script>
+  $(document).ready(function () {
+    $('#crear').DataTable();
+  });
+</script>
