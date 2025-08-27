6db823b21b0796cd0aef3dc002dfb32055df30b0
Marcos <marcossantinomazzanti@gmail.com>
2025-08-27 20:23:31 -0300

    Se modifico la pagina consulta. Se removio el form web3 y se agrego una recomendacion para loggearse/registrarse

--- app/Views/Contacto.php
+++ app/Views/Contacto.php
@@ -6,20 +6,6 @@
     <p>En Xport estamos para ayudarte a encontrar el auto de tus sueños. 
        Si tenés consultas, querés agendar una visita o simplemente necesitás más información, podés comunicarte a través de los siguientes medios:</p>
 
-    <div class="form-container">
-    <form action="https://api.web3forms.com/submit" method="POST" class="contact-right">
-        <div class="contact-right-title">
-        <h2 class = "titulos-contacto">Contactanos</h2>
-        <hr>
-        </div>
-        <input type="hidden" name="access_key" value="bf818953-ca3a-4d60-8224-8346c9b5770d">
-        <input type="text" name="Nombre" placeholder="Tu nombre" class="contact-inputs" required>
-        <input type="email" name="Correo" placeholder="Tu correo" class="contact-inputs" required>
-        <textarea name="message" placeholder="Tu consulta" class="contact-inputs" required></textarea>
-        <button type="submit">Enviar</button>
-    </form>
-    </div>
-
 <br>
     <h2 class = "titulos-contacto">Medios de Contacto</h2>
     <ul style="list-style: none; padding: 0;">
@@ -35,6 +21,9 @@
         <li><strong>Titular: </strong>Marcos Santino Mazzanti y Joaquin Rojas Yacuzzi</li>
         <li><strong>Domicilio Legal:</strong> Yrigoyen 128</li>
     </ul>
+
+    <h3 class = "titulos-contacto">Consultas</h3>
+    <p>Si deseas hacer una <strong>consulta específica</strong>, te recomendamos registrarte o iniciar sesión, lo que habilitará una sección donde podrás contarnos tu duda.</p>
     <p>También te invitamos a visitarnos personalmente en nuestro local,
        donde podrás ver todos los modelos disponibles y recibir atención personalizada de nuestro equipo especializado. ¡Te esperamos!</p>
 </div>
