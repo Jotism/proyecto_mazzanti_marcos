7a8003efb4970bc07ae4583a2650a89ca08f35cc
Marcos <marcossantinomazzanti@gmail.com>
2025-09-10 14:54:53 -0300

    productos sin stock aparecen como tal en el catalogo

--- app/Views/Catalogo.php
+++ app/Views/Catalogo.php
@@ -25,29 +25,34 @@
     <div class="row">
         <?php if (!empty($productos)): ?>
             <?php foreach ($productos as $row): ?>
-                <?php if ($row['eliminado'] == "NO" && $row['stock'] > $row['stock_min']): ?>
+                <?php if ($row['eliminado'] == "NO"): ?>
                     <div class="col-md-4 mb-4">
                         <div class="card h-100">
                             <img src="<?= base_url('assets/uploads/' . $row['imagen']) ?>" class="card-img-top" alt="<?= $row['nombre_prod'] ?>" style="height: 200px; object-fit: cover;">
                             <div class="card-body">
                                 <h5 class="card-title"><?= $row['nombre_prod'] ?></h5>
-                                <p class="card-text">$<?= number_format($row['precio_vta'], 2, ',', '.') ?></p>
+                                <?php if ($row['stock'] > 0): ?>
+                                    <p class="card-text">$<?= number_format($row['precio_vta'], 2, ',', '.') ?></p>
 
-                                <?= form_open('carrito/add'); ?>
-                                    <?= form_hidden('id', $row['id']); ?>
-                                    <?= form_hidden('precio_vta', $row['precio_vta']); ?>
-                                    <?= form_hidden('nombre_prod', $row['nombre_prod']); ?>
-                                    <?= form_hidden('imagen', $row['imagen']); ?>
+                                    <?= form_open('carrito/add'); ?>
+                                        <?= form_hidden('id', $row['id']); ?>
+                                        <?= form_hidden('precio_vta', $row['precio_vta']); ?>
+                                        <?= form_hidden('nombre_prod', $row['nombre_prod']); ?>
+                                        <?= form_hidden('imagen', $row['imagen']); ?>
 
-                                    <?php
-                                    $btn = array(
-                                        'class' => 'btn btn-primary btn-block fuenteBotones',
-                                        'value' => 'Agregar al Carrito',
-                                        'name' => 'action'
-                                    );
-                                    echo form_submit($btn);
-                                    echo form_close();
-                                    ?>
+                                        <?php
+                                        $btn = array(
+                                            'class' => 'btn btn-primary btn-block fuenteBotones',
+                                            'value' => 'Agregar al Carrito',
+                                            'name' => 'action'
+                                        );
+                                        echo form_submit($btn);
+                                        echo form_close();
+                                        ?>
+                                <?php else: ?>
+                                    <br>
+                                    <h3 class="card-text text-danger">*PRODUCTO SIN STOCK*</h3>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
